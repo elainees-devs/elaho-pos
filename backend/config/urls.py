e76573fbd812244ad7e2b584d938a331d10add26
elainees-devs <elainees.devs@gmail.com>
2026-07-07 19:36:58 +0300

    feat(api): add v1 versioned routes for users and roles

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,24 +1,3 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views.
-For more information please see:
-https://docs.djangoproject.com/en/5.2/topics/http/urls/
-
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import include, path
 
@@ -26,9 +5,10 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    # Roles API endpoints
-    path(
-        "api/",
-        include("apps.roles.urls")
-    ),
+    # ------------------------------------------------------------------
+    # Version 1 API Endpoints
+    # ------------------------------------------------------------------
+
+    path("api/v1/roles/", include("apps.roles.urls")),
+    path("api/v1/users/", include("apps.users.urls")),
 ]
