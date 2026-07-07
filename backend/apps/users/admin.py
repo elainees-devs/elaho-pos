cf871f813a246fd7518289a6a4c61653ad8e2e14
elainees-devs <elainees.devs@gmail.com>
2026-07-07 14:04:28 +0300

    feat(users): register User model in Django admin

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name","email","phone", "role","is_active","created_at" ,"updated_at", "is_deleted_at")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("is_active","role")
