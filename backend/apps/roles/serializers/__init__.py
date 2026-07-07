3c1e857e6ecb8ecce156cbefc454539574ef0cd6
elainees-devs <elainees.devs@gmail.com>
2026-07-07 17:32:43 +0300

    chore: add RolePermissionSerializer export

--- backend/apps/roles/serializers/__init__.py
+++ backend/apps/roles/serializers/__init__.py
@@ -1,7 +1,9 @@
 from .role import RoleSerializer
 from .permission import PermissionSerializer
+from .role_permission import RolePermissionSerializer
 
 __all__=[
     RoleSerializer,
-    PermissionSerializer
+    PermissionSerializer,
+    RolePermissionSerializer,
 ]
