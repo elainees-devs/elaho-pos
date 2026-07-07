bf24831db48762358c6daf6c90feae3e5f04435b
elainees-devs <elainees.devs@gmail.com>
2026-07-07 16:40:16 +0300

    feat(roles): add routes for role permission API endpoints

---
+++ backend/apps/roles/urls.py
@@ -0,0 +1,31 @@
+from rest_framework.routers import DefaultRouter
+
+from .views import (
+    RoleViewSet,
+    PermissionViewSet,
+    RolePermissionViewSet,
+)
+
+
+router = DefaultRouter()
+
+router.register(
+    r"roles",
+    RoleViewSet,
+    basename="role"
+)
+
+router.register(
+    r"permissions",
+    PermissionViewSet,
+    basename="permission"
+)
+
+router.register(
+    r"role-permissions",
+    RolePermissionViewSet,
+    basename="role-permission"
+)
+
+
+urlpatterns = router.urls
