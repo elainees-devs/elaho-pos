7c0c0ac381c8d75ed05a79deb8e6b73dcc1123a9
elainees-devs <elainees.devs@gmail.com>
2026-07-07 16:36:05 +0300

    feat(roles): export RolePermissionViewSet from views package

--- backend/apps/roles/views/__init__.py
+++ backend/apps/roles/views/__init__.py
@@ -1,8 +1,10 @@
 from .role import RoleViewSet
 from .permission import PermissionViewSet
+from .role_permission import RolePermissionViewSet
 
 
 __all__=[
     RoleViewSet,
-    PermissionViewSet
+    PermissionViewSet,
+    RolePermissionViewSet
 ]
