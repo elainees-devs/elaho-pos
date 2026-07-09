3bd8e24fe6ae23f3d991b3d60972473e255b6338
elainees-devs <elainees.devs@gmail.com>
2026-07-09 15:46:12 +0300

    refactor(services): import RolePermissionService from package

--- backend/apps/roles/views/role_permission.py
+++ backend/apps/roles/views/role_permission.py
@@ -2,7 +2,7 @@ from rest_framework import status, viewsets
 from rest_framework.response import Response
 
 from ..serializers import RolePermissionSerializer
-from ..services.role_permission_service import RolePermissionService
+from ..services import RolePermissionService
 
 
 class RolePermissionViewSet(viewsets.ViewSet):
