ef8dd70f6b567978ae6b3fcded922fa341c01fe2
elainees-devs <elainees.devs@gmail.com>
2026-07-10 09:49:48 +0300

    fix: add CorsMiddleware to MIDDLEWARE list

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
 
+    "corsheaders.middleware.CorsMiddleware",
+
     "django.middleware.security.SecurityMiddleware",
 
     "django.contrib.sessions.middleware.SessionMiddleware",
