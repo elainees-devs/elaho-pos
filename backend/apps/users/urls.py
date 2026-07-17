ad60cde6312455713511e70a52d01bd70d5896dd
elainees-devs <elainees.devs@gmail.com>
2026-07-17 11:56:50 +0300

    feat(users): add user invitation endpoint

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,7 +1,11 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+
+from .views import InviteUserView, UserViewSet
 
 router = DefaultRouter()
 router.register("", UserViewSet, basename="users")
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path("invite/", InviteUserView.as_view(), name="invite_user"),
+]
